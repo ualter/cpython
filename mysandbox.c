f5bdfab7f0b3a20fd26d85c18eae4684d7b653de
Ualter Otoni <ualter.junior@gmail.com>
2023-08-07 13:56:17 +0200

    #sandbox: Sandbox compiling

--- mysandbox.c
+++ mysandbox.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+// #include <Python.h>
 #include "Python.h"
 
 int main(int argc, char const *argv[])
 {
-    PyObject *co;
-    co = Py_CompileString("print('world')","",0);
-    printf("finish\n");
-    printf("%ld",co->ob_refcnt);
+    // PyObject *co;
+    // co = Py_CompileString("print('world')","",0);
+    // printf("finish\n");
+    // printf("%ld",co->ob_refcnt);
+
+    PyStatus status;
+    PyConfig config;
+    PyConfig_InitPythonConfig(&config);
+
+    printf("got to the end\n");
     return 0;
 }
+
+                                                                                                                                                                                                                                                    
