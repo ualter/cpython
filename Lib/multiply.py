2fb5fbffe00c14c83f7c360b4d4ab4d06814a225
Ualter Otoni <ualter.junior@gmail.com>
2023-08-09 23:30:15 +0200

    #sandbox Embedding - Calling a function Module from C

---
+++ Lib/multiply.py
@@ -0,0 +1,8 @@
+__all__ = ["multiply"]
+
+def multiply(a,b):
+    print("Will compute", a, "times", b)
+    c = 0
+    for i in range(0, a):
+        c = c + b
+    return c
---
+++ labs-embedding-python-function-call/__init__.py
@@ -0,0 +1,3 @@
+from multiply import *
+
+__all__ = "multiply"
